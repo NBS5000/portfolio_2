ac1e9b788fcee264068db1d85dd4b87693954677
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-31 18:12:00 +1100

    modal works, pulls links from git api and links successfully added to page, modal styled

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,8 +1,6 @@
 /*
-Project 1
-Date Night
+portfolio_2
 - Steve Barry -
-- Samer Balee - Mona Mahmoud - Joel Shewan -
 Javascript file for handling display of modal screen
 Modal js taken from w3schools*/
 
@@ -17,23 +15,105 @@ var canx = document.getElementById("canx");
 
 // When the user clicks the "Begin"  button, open the modal 
 
-var modalBtn = document.getElementById("modalBtn");
-modalBtn.addEventListener("click",function(){
-    modal.style.display = "block";
+var gitLink = "https://nbs5000.github.io/";
+var linkFrame = document.getElementById("modalView");
+var linkTitle = document.getElementById("modalTitle");
+var linkAbout = document.getElementById("modalAbout");
+var requestUrl = 'https://api.github.com/repos/nbs5000/';
+var repoLink = "https://github.com/NBS5000/";
+
+var dateBtn = document.getElementById("img_dateNight");
+dateBtn.addEventListener("click",function(){
+    var type = "dateNight";
+    var link = gitLink + type;
+    linkFrame.src = link;
+    linkTitle.innerHTML = "The Weather";
+    getInfo(type);
+});
+var weatherBtn = document.getElementById("img_weather");
+weatherBtn.addEventListener("click",function(){
+    var type = "weather";
+    var link = gitLink + type;
+    linkFrame.src = link;
+    linkTitle.innerHTML = "The Weather";
+    getInfo(type);
+});
+var passwordBtn = document.getElementById("img_password");
+passwordBtn.addEventListener("click",function(){
+    var type = "passwordGenerator";
+    var link = gitLink + type;
+    linkFrame.src = link;
+    linkTitle.innerHTML = "Password Generator";
+    getInfo(type);
+});
+var schedulerBtn = document.getElementById("img_scheduler");
+schedulerBtn.addEventListener("click",function(){
+    var type = "scheduler";
+    var link = gitLink + type;
+    linkFrame.src = link;
+    linkTitle.innerHTML = "Scheduler";
+    getInfo(type);
+});
+var quizBtn = document.getElementById("img_quiz");
+quizBtn.addEventListener("click",function(){
+    var type = "quizTime";
+    var link = gitLink + type;
+    linkFrame.src = link;
+    linkTitle.innerHTML = "Quiz Time!";
+    getInfo(type);
 });
 
-var modalBtnAss = document.getElementById("modalBtnAssess");
-modalBtnAss.addEventListener("click",function(){
-    var dateUrl = "./date.html?open=yes";
-    document.location.href = dateUrl;
-});
+function getInfo(repoName){
+
+    var url = requestUrl + repoName;
+    fetch(url)
+    .then(
+        res => res.json(),
+    )
+    .then(function(res){
+
+        var about = res.description;
+        var page = res.homepage;
+        var git = res.html_url;
+        if(!about || !page){
+            setTimeout(function(){},1000);
+        }
+        if(!page){
+            page = gitLink + repoName;
+        }
+        linkAbout.innerHTML = about;
+        linkFrame.src = page;
+
+        document.getElementById("visit").value = repoName;
+        document.getElementById("git").value = repoName;
+        
+        modal.style.display = "block";
+    })
+    .catch(function (error) {
+        alert('About did not work: ' + error);
+    });
+    
+}
+function visitPage(){
+    var goto = document.getElementById("visit").value;
+    var link = gitLink + goto + "/index.html";
+    window.open(link, '_blank').focus();
+}
+function visitGit(){
+    var goto = document.getElementById("visit").value;
+    var link = repoLink + goto;
+    window.open(link, '_blank').focus();
+}
 
 // When the user clicks the "Cancel" button, close the modal and clear fields
 
 var canxBtn = document.getElementById("canx");
 canxBtn.addEventListener("click",function(){
-    document.getElementById("search").value = "";
-    document.getElementById("datePicker").value = "";
+    linkAbout.innerHTML = "";
+    linkTitle.innerHTML = "";
+    document.getElementById("visit").value = "";
+    document.getElementById("git").value = "";
+    linkFrame.src = "";
     modal.style.display = "none";
 });
 
