cfc2d3bc867fed2efde33142be412b662b119f1a
NBS5000 <steve.nbs.barry@gmail.com>
2022-01-31 13:55:13 +1100

    code added for cricket js game, css updated with contact icon alignment, beginings of modal structure brought across

---
+++ assets/js/modal.js
@@ -0,0 +1,45 @@
+/*
+Project 1
+Date Night
+- Steve Barry -
+- Samer Balee - Mona Mahmoud - Joel Shewan -
+Javascript file for handling display of modal screen
+Modal js taken from w3schools*/
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var canx = document.getElementById("canx");
+
+// When the user clicks the "Begin"  button, open the modal 
+
+var modalBtn = document.getElementById("modalBtn");
+modalBtn.addEventListener("click",function(){
+    modal.style.display = "block";
+});
+
+var modalBtnAss = document.getElementById("modalBtnAssess");
+modalBtnAss.addEventListener("click",function(){
+    var dateUrl = "./date.html?open=yes";
+    document.location.href = dateUrl;
+});
+
+// When the user clicks the "Cancel" button, close the modal and clear fields
+
+var canxBtn = document.getElementById("canx");
+canxBtn.addEventListener("click",function(){
+    document.getElementById("search").value = "";
+    document.getElementById("datePicker").value = "";
+    modal.style.display = "none";
+});
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
